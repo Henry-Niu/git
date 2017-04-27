d9ad42304b0726989d6ec2f078e18b9fdebb6458
Henry-Niu <niuhong2@163.com>
2017-04-28 07:44:34 +0800

    add endl

--- Administrator.hpp
+++ Administrator.hpp
@@ -2,21 +2,30 @@ class Administrator{
 private:
 public:
 	Administrtor(){
+		
 	}
 	~Administrtor(){
+		
 	}
 	void Add_Train(){
+		
 	}
 	void Change_Train(){
+		
 	}
 	void Delete_Train(){
+		
 	}
 	void Start_Train(){
+		
 	}
 	void Stop_Train(){
+		
 	}
 	void Check_User(){
+		
 	}
 	void Check_System_log(){
+		
 	} 
 };

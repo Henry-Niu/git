16610e05039c051a8248e1cb60d05d1bd23bdc80
Henry-Niu <niuhong2@163.com>
2017-04-27 21:57:04 +0800

    add Administrator.hpp

---
+++ Administrator.hpp
@@ -0,0 +1,22 @@
+class Administrator{
+private:
+public:
+	Administrtor(){
+	}
+	~Administrtor(){
+	}
+	void Add_Train(){
+	}
+	void Change_Train(){
+	}
+	void Delete_Train(){
+	}
+	void Start_Train(){
+	}
+	void Stop_Train(){
+	}
+	void Check_User(){
+	}
+	void Check_System_log(){
+	} 
+};
